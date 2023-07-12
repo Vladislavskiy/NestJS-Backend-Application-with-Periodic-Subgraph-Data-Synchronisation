4b9c5d738ea7aea29d6206b92d6a851e0ab6b5b7
Владислав Соловьев <vladislavsolovev@Vladislavs-MacBook-Pro.local>
2023-07-12 12:00:27 +0300

    added constants, added logger

---
+++ src/constants.ts
@@ -0,0 +1,1 @@
+export const UNISWAP_V3_URL = 'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3'
--- src/pool/pool.service.ts
+++ src/pool/pool.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, Logger } from '@nestjs/common'
 import { HttpService } from '@nestjs/axios'
 import { type Prisma } from '@prisma/client'
 import { Cron, CronExpression } from '@nestjs/schedule'
@@ -7,31 +7,38 @@ import { type AxiosResponse } from 'axios'
 import { type IPoolResponse } from 'src/interfaces/pool.interface'
 import { PrismaService } from 'src/prisma.service'
 import { type IPool } from '../interfaces/pool.interface'
+import { UNISWAP_V3_URL } from 'src/constants'
 
 @Injectable()
 export class PoolService {
+  private readonly logger = new Logger(PoolService.name)
+
   constructor (private readonly httpService: HttpService, private readonly prisma: PrismaService) {}
 
   @Cron(CronExpression.EVERY_10_SECONDS)
   async handleCron (): Promise<void> {
     this.fetch().subscribe({
       next: async (pools: IPool[]) => {
+        this.logger.log('Pools fetched successfully')
+
         try {
           await Promise.all(
             pools.map(async (pool) => {
               await this.upsertPool(pool)
             })
           )
+
+          this.logger.log('Pools upserted successfully')
         } catch (err) {
-          console.log(err)
+          this.logger.error(`Failed to upsert: ${err.message}`)
         }
       },
-      error: err => { console.log(err) }
+      error: err => { this.logger.error(`Failed to fetch: ${err.message}`) }
     })
   }
 
   fetch (): Observable<Prisma.PoolCreateInput[]> {
-    return this.httpService.post('https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3', {
+    return this.httpService.post(UNISWAP_V3_URL, {
       query: `
               {
                 pools(first:10){
@@ -50,7 +57,7 @@ export class PoolService {
     }))
   }
 
-  async upsertPool (pool: IPool): Promise<any> {
+  async upsertPool (pool: IPool): Promise<IPool> {
     return await this.prisma.pool.upsert({
       where: { pool: pool.pool },
       update: pool,
