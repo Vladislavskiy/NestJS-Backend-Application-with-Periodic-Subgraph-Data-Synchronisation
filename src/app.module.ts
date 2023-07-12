16e59f687578985ea59a42795c201de1237acc8f
Владислав Соловьев <vladislavsolovev@Vladislavs-MacBook-Pro.local>
2023-07-12 08:10:08 +0300

    got rid of controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PoolModule } from './pool/pool.module';
 
 @Module({
   imports: [PoolModule, ConfigModule.forRoot()],
-  controllers: [AppController],
+  controllers: [],
   providers: [AppService],
 })
 export class AppModule {}
--- src/pool/pool.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { PoolService } from './pool.service';
-
-@Controller('pool')
-export class PoolController {
-    constructor(private readonly poolService: PoolService) {}
-}
